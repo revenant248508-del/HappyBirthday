54cc62e4cab1ee9f9b9bc15cfed30f0150adff3d
revenant248508-del <revenant.2.4.8.5.0.8@gmail.com>
2026-02-04 15:18:31 +0800

    CakeScreen.jsx

--- src/components/screens/CakeScreen.jsx
+++ src/components/screens/CakeScreen.jsx
@@ -46,7 +46,6 @@ export default function CakeScreen({ onNext, onDecorate }) {
           transition={{ duration: 1, ease: "easeOut", delay: 1.5 }}
         >
           Happy Birthday, Cutiepie!
-          Happy Birthday HANNN😘😘
         </motion.div>
       )}
 
